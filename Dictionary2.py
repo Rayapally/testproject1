dbd6a82e58fe4051308fad062a1bc4b2084a628a
rayapallgit <rayapallyvikas1@gmail.com>
2023-07-01 19:36:51 +0530

    dictionaries with function example

---
+++ Dictionary2.py
@@ -0,0 +1,24 @@
+'''countries = {"America":"texas","italy":"rome","india":"andhra pradesh"}
+
+for i in countries:
+    print(i)'''
+
+def countries():
+    countries = {"india":"Andhra Prades","America":"Texas","Australia":"Sydney"}
+
+    print(countries)
+
+
+print(countries())
+
+
+
+
+
+
+
+
+
+
+
+

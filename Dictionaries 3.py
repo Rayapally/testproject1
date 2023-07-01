39248af35c7c6700e47471d5b9be34213b44537d
rayapallgit <rayapallyvikas1@gmail.com>
2023-07-01 19:44:51 +0530

    dictionaries3 with function example

---
+++ Dictionaries 3.py
@@ -0,0 +1,12 @@
+def countries(dictionary):
+    for key,value in dictionary.items():
+        print(f"{key}:{value}")
+
+countries_cap={"America":"Texas","India":"Delhi"}
+
+countries(countries_cap)
+
+fruit_color = {"Apple":"Red","Mango":"yellow"}
+
+countries(fruit_color)
+

afaf2ef8b5150998d03af2aedefe7b65aa8becea
rayapallgit <rayapallyvikas1@gmail.com>
2023-07-05 00:37:08 +0530

    commit

--- lastdigit.py
+++ lastdigit.py
@@ -1,14 +1,19 @@
 x = int(input("enter number:"))
 sum = 0
+temp = x
 
 
-while(x != 0):
+while(x > 0):
 
-    res = x % 10
+    res = x % 10 #it will give last digit
     sum = sum *10 + res
     x //= 10
-
-
-
-
+# it will give --- reverse number
 print(str(sum))
+
+# it will give the  pallindrome or not
+if(temp == sum):
+    print("is pallindrome")
+else:
+    print("it was not pallindrome")
+

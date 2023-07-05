300883ba83980feccd39697a5bb89ad4b9f809fa
rayapallgit <rayapallyvikas1@gmail.com>
2023-07-05 17:02:35 +0530

    upload

---
+++ Absolutefunction.py
@@ -0,0 +1,21 @@
+'''x = int(input("enter your number:"))
+
+ld = abs(x) % 10
+print(ld)'''
+
+def abs_digit(num):
+
+    ld = abs(num) % 10
+    print(ld)
+
+num = int(input("enter the number:"))
+abs_digit(num)
+
+'''def get_last_digit(num):
+    ld = abs(num) % 10
+    print(ld)
+
+num = int(input("Enter the number: "))
+get_last_digit(num)'''
+
+
